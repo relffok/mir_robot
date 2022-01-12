1a97c709c441954832e5d79cd38ff88e1c7f0191
niemsoen <soenke.niemann@googlemail.com>
2022-01-12 12:04:48 +0100

    trying to fix import

--- mir_driver/mir_driver/mir_bridge.py
+++ mir_driver/mir_driver/mir_bridge.py
@@ -11,7 +11,7 @@ import sys
 from collections.abc import Iterable
 
 import mir_driver.rosbridge
-from mir_restapi.mir_restapi.mir_restapi_client import MirRestAPIClient
+from mir_restapi_client import MirRestAPIClient
 from rclpy_message_converter import message_converter
 from geometry_msgs.msg import TwistStamped
 from nav_msgs.msg import Odometry
